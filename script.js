ecb9adce11ccd12d6422c176a9d1f1e2ec863617
ivanow12 <bbeshetya@gmail.com>
2024-06-21 21:16:11 +0300

    dobavil dz

---
+++ script.js
@@ -0,0 +1,33 @@
+let destination = "Бодрум";
+let duration = 7;
+let price = 400;
+let transfer = true;
+console.log(`Место назначения: ${destination} /`, `Продолжительность: ${duration} дней /`, `Стоимость ${price}$ /`, `Трансфер ${transfer}`) 
+
+
+let name1 = "Туле";
+let riches = "Золото и бриалинты";
+let tribe = 4;
+let invisible = false;
+console.log(`название: ${name1} /`, `наличие сокровищ: ${riches} /` , `количество племен: ${tribe} /`, `невидынные существа:${invisible} /`)
+
+
+let codeName = "Зонт";
+let level = 3;
+let progress = 0;
+let fake = false;
+console.log(`Кодовое имя: ${codeName} /` , `Уровень доступа: ${level} /`, `Количество выполненных мисcий: ${progress} /`, `Наличие поддельного паспорта: ${fake}` )
+
+let flightTarget = "изучения луны";
+let fuelConsumption = 800;
+let duration2 = 300;
+let laboratory = true;
+console.log(`Цель полета: ${flightTarget} /`, `Расход топлива: ${fuelConsumption} /`, `Продолжительность в днях: ${duration2} /`, `Исследовательская лаборатория: ${laboratory}.` ) 
+
+let nameRob = "Чапи";
+let levelRob = 4;
+let ability = "Да"
+let autonomousWork = true;
+console.log(`Имя робота: ${nameRob} /`, `Уровень ИИ: ${levelRob} /` , `Возможность обучения: ${autonomousWork}.`)
+
+
